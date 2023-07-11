b8e9562dc415b5ade86037b4d13f196b06001e6e
kushoon <abhishek.serjam@gmail.com>
2023-07-11 13:27:38 +0800

    Base application, next is Redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { v4 as uuidv4 } from "uuid";
 
-function App() {
+export default function App() {
+  const [todo, setToDo] = useState("");
+  const [toDoList, setToDoItems] = useState([]);
+
+  const setToDoList = (e) => {
+    e.preventDefault();
+    setToDoItems([...toDoList, { id: uuidv4(), text: todo, checked: false }]);
+    setToDo("");
+  };
+
+  const checkToDoItem = (id, checked) => {
+    setToDoItems((toDoList) => {
+      return toDoList.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, checked };
+        }
+        return todo;
+      });
+    });
+  };
+
+  const deleteTodo = (id) => {
+    setToDoItems((toDoList) => {
+      return toDoList.filter((todo) => todo.id !== id);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={setToDoList}>
+        <input
+          type="text"
+          value={todo}
+          onChange={(e) => setToDo(e.target.value)}
+        />
+        <button type="submit">Add ToDo</button>
+      </form>
+      <ul>
+        {toDoList.map((todo) => {
+          const checkboxId = `checkbox_${todo.id}`;
+          return (
+            <li key={todo.id}>
+              <input
+                type="checkbox"
+                id={checkboxId}
+                checked={todo.checked}
+                onChange={(e) => checkToDoItem(todo.id, e.target.checked)}
+              />
+              <label className="m-l-10" htmlFor={checkboxId}>
+                {todo.text}
+              </label>
+              <button className="m-l-10" onClick={() => deleteTodo(todo.id)}>
+                delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
-
-export default App;
